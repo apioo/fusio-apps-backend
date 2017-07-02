c8135bfb254da5637443653f16d8b1eb240e74a8
Christoph Kappestein <christoph.kappestein@gmail.com>
2017-07-02 22:27:01 +0200

    add audit test

---
+++ tests/app/audit.js
@@ -0,0 +1,55 @@
+'use strict';
+
+describe('Audit tests', function() {
+
+  it('List audit', function() {
+    browser.get('#/audit');
+
+    var EC = protractor.ExpectedConditions;
+
+    // set filter settings
+    $('a.btn-primary').click();
+
+    browser.wait(EC.visibilityOf($('div.modal-body')), 5000);
+
+    element(by.model('filter.from')).clear().sendKeys('2015-06-01');
+    element(by.model('filter.to')).clear().sendKeys('2015-06-30');
+
+    $('button.btn-primary').click();
+
+    var audits = element.all(by.repeater('audit in audits').column('audit.event'));
+    expect(audits.count()).toEqual(2);
+    expect(audits.get(0).getText()).toMatch('/bar');
+    expect(audits.get(1).getText()).toMatch('/bar');
+  });
+
+  it('Detail audit', function() {
+    browser.get('#/audit');
+
+    var EC = protractor.ExpectedConditions;
+
+    // set filter settings
+    $('a.btn-primary').click();
+
+    browser.wait(EC.visibilityOf($('div.modal-body')), 5000);
+
+    element(by.model('filter.from')).clear().sendKeys('2015-06-01');
+    element(by.model('filter.to')).clear().sendKeys('2015-06-30');
+
+    $('button.btn-primary').click();
+
+    element.all(by.css('div.fusio-options a:nth-child(1)')).get(1).click();
+
+    browser.wait(EC.visibilityOf($('div.modal-body')), 5000);
+
+    expect(element(by.model('audit.app.name')).getText()).toEqual('');
+    expect(element(by.model('audit.user.name')).getText()).toEqual('');
+    expect(element(by.model('audit.refId')).getText()).toEqual('');
+    expect(element(by.model('audit.event')).getText()).toEqual('');
+    expect(element(by.model('audit.ip')).getText()).toEqual('');
+    expect(element(by.model('audit.message')).getText()).toEqual('');
+
+    $('button.btn-default').click();
+  });
+
+});
