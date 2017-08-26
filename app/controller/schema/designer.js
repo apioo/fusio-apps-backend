b8c3d1964451ae3df49c304d588103fdb7d77667
Christoph Kappestein <christoph.kappestein@gmail.com>
2017-08-26 02:14:57 +0200

    update uib alert and hash prefix

--- app/controller/schema/designer.js
+++ app/controller/schema/designer.js
@@ -26,7 +26,7 @@ module.exports = function($scope, $http, $routeParams, fusio) {
     $http.post(fusio.baseUrl + 'backend/schema/preview/' + schemaId, null)
       .then(function(response) {
         var data = response.data;
-        data.preview = data.preview.replace(/href=\"\#([A-z0-9_]+)\"/g, "href=\"#/schema/designer/" + schemaId + "\"");
+        data.preview = data.preview.replace(/href=\"\#([A-z0-9_]+)\"/g, "href=\"#!/schema/designer/" + schemaId + "\"");
         $scope.response = data;
       });
   };
