15dd28c413fba0f37eac32c272baf660895e9482
Christoph Kappestein <christoph.kappestein@gmail.com>
2018-05-31 22:03:09 +0200

    add event panel

---
+++ app/controller/event/create.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = function($scope, $http, $uibModalInstance, fusio) {
+
+  $scope.event = {
+    name: '',
+    description: ''
+  };
+
+  $scope.create = function(event) {
+    var data = angular.copy(event);
+
+    $http.post(fusio.baseUrl + 'backend/event', data)
+      .then(function(response) {
+        var data = response.data;
+        $scope.response = data;
+        if (data.success === true) {
+          $uibModalInstance.close(data);
+        }
+      })
+      .catch(function(response) {
+        $scope.response = response.data;
+      });
+  };
+
+  $scope.close = function() {
+    $uibModalInstance.dismiss('cancel');
+  };
+
+  $scope.closeResponse = function() {
+    $scope.response = null;
+  };
+
+};
