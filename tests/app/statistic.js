7673a216a227ce71390e8adecec813ffcb04bc99
Christoph Kappestein <christoph.kappestein@gmail.com>
2018-10-11 15:42:38 +0200

    fix statistic test

--- tests/app/statistic.js
+++ tests/app/statistic.js
@@ -6,16 +6,18 @@ describe('Statistic tests', function() {
     browser.get('#!/statistic');
 
     var routes = element.all(by.options('statistic.value as statistic.name for statistic in statistics'));
-    expect(routes.count()).toEqual(7);
-    expect(routes.get(0).getText()).toEqual('Incoming requests');
-    expect(routes.get(1).getText()).toEqual('Most used routes');
-    expect(routes.get(2).getText()).toEqual('Most used apps');
-    expect(routes.get(3).getText()).toEqual('Errors per route');
-    expect(routes.get(4).getText()).toEqual('Issued tokens');
-    expect(routes.get(5).getText()).toEqual('Time average');
-    expect(routes.get(6).getText()).toEqual('Time per route');
+    expect(routes.count()).toEqual(9);
+    expect(routes.get(0).getText()).toEqual('Errors per route');
+    expect(routes.get(1).getText()).toEqual('Incoming requests');
+    expect(routes.get(2).getText()).toEqual('Incoming transactions');
+    expect(routes.get(3).getText()).toEqual('Issued tokens');
+    expect(routes.get(4).getText()).toEqual('Most used apps');
+    expect(routes.get(5).getText()).toEqual('Most used routes');
+    expect(routes.get(6).getText()).toEqual('Time average');
+    expect(routes.get(7).getText()).toEqual('Time per route');
+    expect(routes.get(8).getText()).toEqual('Used points');
 
-    for (var i = 0; i <= 6; i++) {
+    for (var i = 0; i <= 8; i++) {
       routes.get(i).click();
       browser.waitForAngular();
     }
