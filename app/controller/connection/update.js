20cffa12f0a1e37f562f7a58319d85a2282dba64
Christoph Kappestein <christoph.kappestein@gmail.com>
2021-10-30 20:48:15 +0200

    add button to handle authorization

--- app/controller/connection/update.js
+++ app/controller/connection/update.js
@@ -2,7 +2,7 @@
 
 var angular = require('angular')
 
-module.exports = function ($scope, $http, $uibModalInstance, fusio, formBuilder, connection) {
+module.exports = function ($scope, $http, $uibModalInstance, $window, fusio, formBuilder, connection) {
   $scope.connection = connection
   $scope.elements = []
   $scope.config = {}
@@ -31,6 +31,25 @@ module.exports = function ($scope, $http, $uibModalInstance, fusio, formBuilder,
 
   $scope.close = function () {
     $uibModalInstance.dismiss('cancel')
+  }
+
+  $scope.authorize = function () {
+    $http.get(fusio.baseUrl + 'backend/connection/' + connection.id + '/redirect')
+      .then(function (response) {
+        var data = response.data;
+        if (data.redirectUri) {
+          $window.location.href = data.redirectUri;
+          return;
+        }
+
+        $scope.response = data
+        if (data.success === true) {
+          $uibModalInstance.close(data)
+        }
+      })
+      .catch(function (response) {
+        $scope.response = response.data
+      })
   }
 
   $scope.closeResponse = function () {
