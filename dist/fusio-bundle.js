422c8ced3d87b0d9e9aa0ea5f1f448399b6240f9
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-03-13 23:06:07 +0100

    update dist

--- dist/fusio-bundle.js
+++ dist/fusio-bundle.js
@@ -7590,6 +7590,9 @@ module.exports = function ($uibModal) {
   var helper = {}
 
   helper.showDialog = function (path) {
+    // replace slash with empty string
+    path = path.replaceAll('/', '')
+
     $uibModal.open({
       size: 'lg',
       template: '<div class="modal-body"><iframe src="https://docs.fusio-project.org/docs/backend/' + path + '" width="100%" height="600px"></iframe></div>'
