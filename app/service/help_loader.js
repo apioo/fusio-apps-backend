21159867a93c0ec53086f65fb2faec1e4bea0cf2
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-05-12 19:55:25 +0200

    fix loading help

--- app/service/help_loader.js
+++ app/service/help_loader.js
@@ -4,9 +4,6 @@ module.exports = function ($uibModal) {
   var helper = {}
 
   helper.showDialog = function (path) {
-    // replace slash with empty string
-    path = path.replaceAll('/', '')
-
     $uibModal.open({
       size: 'lg',
       template: '<div class="modal-body"><iframe src="https://docs.fusio-project.org/docs/backend/' + path + '" width="100%" height="600px"></iframe></div>'
