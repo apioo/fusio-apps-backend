f805cbf15e32cd3bbb8a0175a6a50820b03885a9
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-06-24 20:14:21 +0200

    update token list

--- src/app/token/list/list.component.ts
+++ src/app/token/list/list.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {List} from "../../list";
+import {App_Token} from "fusio-sdk/dist/src/generated/backend/App_Token";
+import {Collection_Category_Query} from "fusio-sdk/src/generated/backend/Collection_Category_Query";
+import {AxiosResponse} from "axios";
+import {Collection} from "fusio-sdk/src/generated/backend/Collection";
+import {DetailComponent} from "../detail/detail.component";
 
 @Component({
-  selector: 'app-list',
+  selector: 'app-token-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent extends List<App_Token> {
 
-  constructor() { }
+  protected async getAll(query: Collection_Category_Query): Promise<AxiosResponse<Collection<App_Token>>> {
+    const group = await this.factory.getClient().backendApp();
+    return await group.getBackendAppToken().backendActionAppTokenGetAll(query);
+  }
 
-  ngOnInit(): void {
+  protected async get(id: string): Promise<AxiosResponse<App_Token>> {
+    const group = await this.factory.getClient().backendApp();
+    return await group.getBackendAppTokenByTokenId(id).backendActionAppTokenGet();
+  }
+
+  protected getDetailComponent(): any {
+    return DetailComponent;
+  }
+
+  protected getRoute(): any {
+    return '/token';
+  }
+
+  protected onList() {
+  }
+
+  protected onGet(): void {
   }
 
 }
