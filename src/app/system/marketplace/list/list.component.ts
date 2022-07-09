8679aad91a075c9cc43b887b9f4ffba222747cf2
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-07-09 16:53:00 +0200

    update marketplace

--- src/app/system/marketplace/list/list.component.ts
+++ src/app/system/marketplace/list/list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FactoryService} from "../../../factory.service";
+import {Message} from "fusio-sdk/dist/src/generated/backend/Message";
+import axios from "axios";
+import {Marketplace_Collection_Apps} from "fusio-sdk/dist/src/generated/backend/Marketplace_Collection_Apps";
 
 @Component({
   selector: 'app-list',
@@ -7,9 +11,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(protected factory: FactoryService) { }
 
-  ngOnInit(): void {
+  working: boolean = false;
+  public apps?: Marketplace_Collection_Apps;
+  public response?: Message;
+
+  async ngOnInit(): Promise<void> {
+    try {
+      const group = await this.factory.getClient().backendMarketplace();
+      const response = await group.getBackendMarketplace().backendActionMarketplaceGetAll();
+
+      this.apps = response.data.apps;
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response)  {
+        this.response = error.response.data as Message;
+      } else {
+        throw error;
+      }
+    }
   }
 
+  async install(appName: string) {
+    try {
+      const group = await this.factory.getClient().backendMarketplace();
+      const response = await group.getBackendMarketplace().backendActionMarketplaceInstall({
+        name: appName
+      });
+
+      //this.response = response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response)  {
+        this.response = error.response.data as Message;
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  async update(appName: string) {
+    try {
+      const group = await this.factory.getClient().backendMarketplace();
+      const response = await group.getBackendMarketplaceByAppName(appName).backendActionMarketplaceUpdate();
+
+      this.response = response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response)  {
+        this.response = error.response.data as Message;
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  async remove(appName: string) {
+    try {
+      const group = await this.factory.getClient().backendMarketplace();
+      const response = await group.getBackendMarketplaceByAppName(appName).backendActionMarketplaceRemove();
+
+      this.response = response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response)  {
+        this.response = error.response.data as Message;
+      } else {
+        throw error;
+      }
+    }
+  }
 }
