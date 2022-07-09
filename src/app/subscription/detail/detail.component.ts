130b0c6f53506878e6ac459161a649c4a20dd9ea
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-07-09 10:24:40 +0200

    update subscription

--- src/app/subscription/detail/detail.component.ts
+++ src/app/subscription/detail/detail.component.ts
@@ -3,6 +3,8 @@ import {AxiosResponse} from "axios";
 import {Message} from "fusio-sdk/dist/src/generated/backend/Message";
 import {Event_Subscription} from "fusio-sdk/dist/src/generated/backend/Event_Subscription";
 import {Detail} from "../../detail";
+import {User} from "fusio-sdk/dist/src/generated/backend/User";
+import {Event} from "fusio-sdk/dist/src/generated/backend/Event";
 
 @Component({
   selector: 'app-subscription-detail',
@@ -10,6 +12,14 @@ import {Detail} from "../../detail";
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent extends Detail<Event_Subscription> {
+
+  events?: Array<Event>;
+  users?: Array<User>;
+
+  override async ngOnInit(): Promise<void> {
+    this.loadEvents();
+    this.loadUsers();
+  }
 
   protected async create(entity: Event_Subscription): Promise<AxiosResponse<Message>> {
     const group = await this.factory.getClient().backendEvent();
@@ -30,6 +40,18 @@ export class DetailComponent extends Detail<Event_Subscription> {
     return {
       endpoint: ''
     };
+  }
+
+  private async loadEvents() {
+    const event = await this.factory.getClient().backendEvent();
+    const response = await event.getBackendEvent().backendActionEventGetAll({count: 1024});
+    this.events = response.data.entry;
+  }
+
+  private async loadUsers() {
+    const user = await this.factory.getClient().backendUser();
+    const response = await user.getBackendUser().backendActionUserGetAll({count: 1024});
+    this.users = response.data.entry;
   }
 
 }
