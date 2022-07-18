7955f464634edc11984c7dd927c8001d8c45107f
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-07-18 23:29:17 +0200

    update sdk

--- src/app/api/generator/generator.component.ts
+++ src/app/api/generator/generator.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {Route_Index_Provider} from "fusio-sdk/dist/src/generated/backend/Route_Index_Provider";
-import {Route_Provider} from "fusio-sdk/dist/src/generated/backend/Route_Provider";
 import {Form_Container} from "fusio-sdk/dist/src/generated/backend/Form_Container";
 import {Message} from "fusio-sdk/dist/src/generated/backend/Message";
-import {Route_Provider_Changelog} from "fusio-sdk/dist/src/generated/backend/Route_Provider_Changelog";
 import {FactoryService} from "../../factory.service";
 import axios from "axios";
+import {Generator_Provider_Changelog} from "fusio-sdk/dist/src/generated/backend/Generator_Provider_Changelog";
+import {Generator_Index_Provider} from "fusio-sdk/dist/src/generated/backend/Generator_Index_Provider";
+import {Generator_Provider} from "fusio-sdk/dist/src/generated/backend/Generator_Provider";
 
 @Component({
   selector: 'app-generator',
@@ -14,8 +14,8 @@ import axios from "axios";
 })
 export class GeneratorComponent implements OnInit {
 
-  providers?: Array<Route_Index_Provider>;
-  provider: Route_Provider = {
+  providers?: Array<Generator_Index_Provider>;
+  provider: Generator_Provider = {
     path: '',
     scopes: [],
     config: {},
@@ -23,13 +23,13 @@ export class GeneratorComponent implements OnInit {
   selected?: string;
   form?: Form_Container;
   response?: Message;
-  changelog?: Route_Provider_Changelog;
+  changelog?: Generator_Provider_Changelog;
 
   constructor(protected factory: FactoryService) { }
 
   async ngOnInit(): Promise<void> {
-    const route = await this.factory.getClient().backendRoute();
-    const response = await route.getBackendRoutesProvider().backendActionRouteProviderIndex();
+    const route = await this.factory.getClient().backendGenerator();
+    const response = await route.getBackendGenerator().backendActionGeneratorIndex();
     if (response.data.providers) {
       this.providers = response.data.providers;
     }
@@ -40,8 +40,8 @@ export class GeneratorComponent implements OnInit {
       return;
     }
 
-    const action = await this.factory.getClient().backendRoute();
-    const response = await action.getBackendRoutesProviderByProvider(this.selected).backendActionRouteProviderForm();
+    const action = await this.factory.getClient().backendGenerator();
+    const response = await action.getBackendGeneratorByProvider(this.selected).backendActionGeneratorForm();
     this.form = response.data;
     this.provider.config = {};
   }
@@ -52,8 +52,8 @@ export class GeneratorComponent implements OnInit {
     }
 
     try {
-      const action = await this.factory.getClient().backendRoute();
-      const response = await action.getBackendRoutesProviderByProvider(this.selected).backendActionRouteProviderChangelog(this.provider.config);
+      const action = await this.factory.getClient().backendGenerator();
+      const response = await action.getBackendGeneratorByProvider(this.selected).backendActionGeneratorChangelog(this.provider.config);
       this.changelog = response.data;
       this.response = undefined;
     } catch (error) {
@@ -71,8 +71,8 @@ export class GeneratorComponent implements OnInit {
     }
 
     try {
-      const route = await this.factory.getClient().backendRoute();
-      const response = await route.getBackendRoutesProviderByProvider(this.selected).backendActionRouteProviderCreate(this.provider)
+      const route = await this.factory.getClient().backendGenerator();
+      const response = await route.getBackendGeneratorByProvider(this.selected).backendActionGeneratorCreate(this.provider)
       this.response = response.data;
     } catch (error) {
       if (axios.isAxiosError(error) && error.response)  {
