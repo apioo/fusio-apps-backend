3b2360dd6b9074dd34374e825c2e2bfd9a2a7abb
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-10-25 23:25:36 +0200

    fix replace

--- nginx/replace_env.sh
+++ nginx/replace_env.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-sed -i 's/fusio\///g' /usr/share/index.html > /usr/share/nginx/html/index.html
-envsubst < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html
+envsubst < /usr/share/index.html > /usr/share/nginx/html/index.html
+sed -i 's/fusio\///g' /usr/share/nginx/html/index.html
