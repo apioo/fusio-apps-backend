d75a9ead730a7f81376606ed278bbfc436792e55
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-12-06 20:30:21 +0100

    update form

--- src/app/consumer/rate/form/form.component.ts
+++ src/app/consumer/rate/form/form.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {ErrorService, Form, FormAutocompleteComponent, HelpService, MessageComponent} from "ngx-fusio-sdk";
-import {BackendApp, BackendOperation, BackendPlan, BackendRate, BackendUser} from "fusio-sdk";
+import {BackendRate} from "fusio-sdk";
 import {RateService} from "../../../services/rate.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {OperationService} from "../../../services/operation.service";
@@ -60,17 +60,39 @@ export class FormComponent extends Form<BackendRate> {
     value: 'No'
   }]
 
-  operations?: Array<BackendOperation>;
-  users?: Array<BackendUser>;
-  plans?: Array<BackendPlan>;
-  apps?: Array<BackendApp>;
-
   constructor(private service: RateService, private help: HelpService, public operation: OperationService, public user: UserService, public plan: PlanService, public app: AppService, route: ActivatedRoute, router: Router, error: ErrorService) {
     super(route, router, error);
   }
 
   protected getService(): RateService {
     return this.service;
+  }
+
+  protected override onLoad() {
+    let timespan = this.entity().timespan;
+    if (!timespan) {
+      return;
+    }
+
+    if (timespan.startsWith('PT')) {
+      timespan = timespan.substring(2);
+      this.timespan.value = parseInt(timespan);
+      if (timespan.endsWith('M')) {
+        this.timespan.unit = 'minute';
+      } else if (timespan.endsWith('H')) {
+        this.timespan.unit = 'hour';
+      }
+    } else if (timespan.startsWith('P')) {
+      timespan = timespan.substring(1);
+      this.timespan.value = parseInt(timespan);
+      if (timespan.endsWith('D')) {
+        this.timespan.unit = 'day';
+      } else if (timespan.endsWith('W')) {
+        this.timespan.unit = 'week';
+      } else if (timespan.endsWith('M')) {
+        this.timespan.unit = 'month';
+      }
+    }
   }
 
   protected override beforeCreate(entity: BackendRate): BackendRate {
@@ -94,6 +116,56 @@ export class FormComponent extends Form<BackendRate> {
     allocation.push({});
 
     this.set(this.entity, 'allocation', allocation);
+  }
+
+  setAllocationOperationId(index: number, operationId: number) {
+    this.entity.update((rate) => {
+      if (!rate.allocation) {
+        return rate;
+      }
+      rate.allocation[index].operationId = operationId;
+      return rate;
+    });
+  }
+
+  setAllocationUserId(index: number, userId: number) {
+    this.entity.update((rate) => {
+      if (!rate.allocation) {
+        return rate;
+      }
+      rate.allocation[index].userId = userId;
+      return rate;
+    });
+  }
+
+  setAllocationPlanId(index: number, planId: number) {
+    this.entity.update((rate) => {
+      if (!rate.allocation) {
+        return rate;
+      }
+      rate.allocation[index].planId = planId;
+      return rate;
+    });
+  }
+
+  setAllocationAppId(index: number, appId: number) {
+    this.entity.update((rate) => {
+      if (!rate.allocation) {
+        return rate;
+      }
+      rate.allocation[index].appId = appId;
+      return rate;
+    });
+  }
+
+  setAllocationAuthenticated(index: number, authenticated: boolean) {
+    this.entity.update((rate) => {
+      if (!rate.allocation) {
+        return rate;
+      }
+      rate.allocation[index].authenticated = authenticated;
+      return rate;
+    });
   }
 
   removeAllocation(index: number) {
