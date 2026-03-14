7b93c3dc0b3fe9469b5463cbdbec820070c8110e
Christoph Kappestein <christoph.kappestein@gmail.com>
2026-03-14 20:34:45 +0100

    optimize uses

--- src/app/api/agent/detail/detail.component.ts
+++ src/app/api/agent/detail/detail.component.ts
@@ -2,7 +2,6 @@ import {Component, signal} from '@angular/core';
 import {Detail, ErrorService, MessageComponent} from "ngx-fusio-sdk";
 import {BackendAgent, BackendAgentTool} from "fusio-sdk";
 import {ActivatedRoute, Router, RouterLink} from "@angular/router";
-import {ActionLinkComponent} from "../../../shared/action-link/action-link.component";
 import {EditorComponent} from "ngx-monaco-editor-v2-alternative";
 import {FormsModule} from "@angular/forms";
 import {JsonPipe} from "@angular/common";
@@ -15,7 +14,6 @@ import {ApiService} from "../../../api.service";
   templateUrl: './detail.component.html',
   imports: [
     MessageComponent,
-    ActionLinkComponent,
     EditorComponent,
     FormsModule,
     JsonPipe,
