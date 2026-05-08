3bd4f51c31de870bdc2b1c6511bba562c69adbcf
Christoph Kappestein <christoph.kappestein@gmail.com>
2026-05-08 23:17:24 +0200

    improve json response formatting and add copy to clipboard button

--- src/app/api/action/designer/response/response.ts
+++ src/app/api/action/designer/response/response.ts
@@ -1,12 +1,14 @@
-import {Component, input} from '@angular/core';
+import {Component, computed, input} from '@angular/core';
 import {BackendActionExecuteResponse} from "fusio-sdk";
-import {JsonPipe, NgClass} from "@angular/common";
+import {NgClass} from "@angular/common";
+import {ClipboardModule} from "ngx-clipboard";
 
 @Component({
   selector: 'app-action-designer-response',
   imports: [
     NgClass,
-    JsonPipe,
+    ClipboardModule,
+
   ],
   templateUrl: './response.html',
   styleUrl: './response.css',
@@ -14,5 +16,19 @@ import {JsonPipe, NgClass} from "@angular/common";
 export class Response {
 
   response = input.required<BackendActionExecuteResponse>();
+
+  body = computed<any>(() => {
+    let body = this.response().body;
+
+    if (typeof body === 'string' && body[0] === '{') {
+      body = JSON.parse(body);
+    }
+
+    if (typeof body === 'string') {
+      return body;
+    }
+
+    return JSON.stringify(body, undefined, 2);
+  });
 
 }
