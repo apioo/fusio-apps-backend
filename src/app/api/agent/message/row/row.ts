1f2465cb3d90a9169f9cea839eea7b3c948e6cb6
Christoph Kappestein <christoph.kappestein@gmail.com>
2026-05-17 13:06:44 +0200

    use markdown for json rendering and add prismjs and katex

--- src/app/api/agent/message/row/row.ts
+++ src/app/api/agent/message/row/row.ts
@@ -1,14 +1,12 @@
 import {Component, input} from '@angular/core';
 import {JsonPipe, NgClass} from "@angular/common";
 import {BackendAgentMessage} from "fusio-sdk";
-import {Highlight} from "ngx-highlightjs";
-import {MarkdownComponent} from "ngx-markdown";
+import {KatexSpecificOptions, MarkdownComponent} from "ngx-markdown";
 
 @Component({
   selector: 'app-agent-message-row',
   imports: [
     JsonPipe,
-    Highlight,
     NgClass,
     MarkdownComponent
   ],
@@ -18,5 +16,9 @@ import {MarkdownComponent} from "ngx-markdown";
 export class Row {
 
   message = input.required<BackendAgentMessage>();
+
+  katexOptions: KatexSpecificOptions = {
+    throwOnError: false,
+  };
 
 }
